ab5aceaaec85894c9df41a272feb1f522e8f27d8
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 15:11:16 -0300

    feat: implement count by user id method

--- src/repositories/in-memory/InMemoryCheckInRepository.ts
+++ src/repositories/in-memory/InMemoryCheckInRepository.ts
@@ -6,6 +6,10 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInRepository implements ICheckInRepository {
   public item: CheckIn[] = []
 
+  async countByUserId(userId: string): Promise<number> {
+    return this.item.filter((checkIn) => checkIn.user_id === userId).length
+  }
+
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
     const checkIns = this.item
       .filter((checkIn) => checkIn.user_id === userId)
