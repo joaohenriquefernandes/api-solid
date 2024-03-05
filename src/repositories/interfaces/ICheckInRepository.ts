c13c7e1e67aba43d32f37ed23d449b722613e7f2
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 10:20:26 -0300

    feat: create find many by user id method

--- src/repositories/interfaces/ICheckInRepository.ts
+++ src/repositories/interfaces/ICheckInRepository.ts
@@ -6,4 +6,5 @@ export interface ICheckInRepository {
     gym_id,
   }: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>
+  findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>
 }
