5bf1d2d9c8920f3032a4b71114ffdb77de22cb98
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 10:21:16 -0300

    feat: implements find many by user id method

--- src/repositories/in-memory/InMemoryCheckInRepository.ts
+++ src/repositories/in-memory/InMemoryCheckInRepository.ts
@@ -6,6 +6,14 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInRepository implements ICheckInRepository {
   public item: CheckIn[] = []
 
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    const checkIns = this.item
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+
+    return checkIns
+  }
+
   async create({
     user_id,
     gym_id,
