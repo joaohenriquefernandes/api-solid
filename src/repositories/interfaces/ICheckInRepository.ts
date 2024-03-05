c25e857c3b67f92cd31902755c1a6144f8da6716
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 15:09:47 -0300

    feat: create count by user id method

--- src/repositories/interfaces/ICheckInRepository.ts
+++ src/repositories/interfaces/ICheckInRepository.ts
@@ -7,4 +7,5 @@ export interface ICheckInRepository {
   }: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>
   findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>
+  countByUserId: (userId: string) => Promise<number>
 }
