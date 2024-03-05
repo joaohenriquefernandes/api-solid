b14d4b99f1657d1cde66088f1a359869fb1441cc
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 18:00:12 -0300

    feat: create save and find by id method

--- src/repositories/interfaces/ICheckInRepository.ts
+++ src/repositories/interfaces/ICheckInRepository.ts
@@ -5,6 +5,8 @@ export interface ICheckInRepository {
     user_id,
     gym_id,
   }: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>
+  save: (checkIn: CheckIn) => Promise<CheckIn>
+  findById: (id: string) => Promise<CheckIn | null>
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>
   findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>
   countByUserId: (userId: string) => Promise<number>
