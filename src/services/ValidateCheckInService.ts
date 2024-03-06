75f7efb548985ebea1042e52e043051946bb6010
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-05 21:39:27 -0300

    feat: create validate check in service

--- src/services/ValidateCheckInService.ts
+++ src/services/ValidateCheckInService.ts
@@ -1,5 +1,7 @@
 import { ICheckInRepository } from '@/repositories/interfaces/ICheckInRepository'
 import { CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/LateCheckInValidationError'
 import { ResourceNotFoundError } from './errors/ResourceNotFoundError'
 
 interface IValidateCheckInServiceRequest {
@@ -20,6 +22,15 @@ export class ValidateCheckInService {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
