900dbed271b5c7850fa01d8744d457896ec0f517
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-06 15:45:18 -0300

    feat: create me route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { RegisterController } from './controllers/RegisterController'
 import { AuthenticateController } from './controllers/AuthenticateController'
+import { ProfileController } from './controllers/ProfileController'
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/register', RegisterController)
   app.post('/sessions', AuthenticateController)
+
+  app.get('/me', ProfileController)
 }
