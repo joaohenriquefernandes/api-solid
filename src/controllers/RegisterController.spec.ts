144cc10d3014fd41739fa0403987567a3daf2dd4
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-08 09:10:50 -0300

    test: create register controller test

---
+++ src/controllers/RegisterController.spec.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/register').send({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
