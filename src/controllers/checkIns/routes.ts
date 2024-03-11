4af81e67b77dc6fb937bc7ba1f5c3bb4f58c4ab7
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-11 09:45:47 -0300

    feat: create  check ins routes

---
+++ src/controllers/checkIns/routes.ts
@@ -0,0 +1,16 @@
+import { verifyJWT } from '@/middlewares/VerifyJWT'
+import { FastifyInstance } from 'fastify'
+import { ValidateController } from './ValidateController'
+import { CreateController } from './CreateController'
+import { HistoryController } from './HistoryController'
+import { MetricsController } from './MetricsController'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms/:gymId/check-ins', CreateController)
+  app.patch('/check-ins/:checkInId/validate', ValidateController)
+
+  app.get('/check-ins/history', HistoryController)
+  app.get('/check-ins/metrics', MetricsController)
+}
