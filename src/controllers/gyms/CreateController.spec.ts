da0a6b1fd995b420532ad15ec82454d88c41d9cd
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-11 14:05:06 -0300

    test: create create controller test

---
+++ src/controllers/gyms/CreateController.spec.ts
@@ -0,0 +1,33 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/createAndAuthenticateUser'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+
+describe('Create Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Gym JS',
+        description: 'Some description',
+        phone: '35999999999',
+        created_at: new Date(),
+        latitude: -21.7843936,
+        longitude: -46.594523,
+      })
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body.gym).toHaveProperty('id')
+  })
+})
