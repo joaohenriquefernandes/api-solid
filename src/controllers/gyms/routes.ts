1cb8c81e18b478d1fe3a36cec2017005edb7e750
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-03-12 14:28:29 -0300

    feat: add middleware in create gym and validate check in routes

--- src/controllers/gyms/routes.ts
+++ src/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { SearchController } from './SearchController'
 import { NearbyController } from './NearbyController'
 import { CreateController } from './CreateController'
+import { verifyUserRole } from '@/middlewares/verifyUserRole'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/gyms', CreateController)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, CreateController)
   app.get('/gyms/search', SearchController)
   app.get('/gyms/nearby', NearbyController)
 }
